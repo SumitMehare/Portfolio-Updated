1c2f9304ae2a05218fdcfad37812f3967942604d
Sumit Mehare <sumitmehare@gmail.com>
2021-11-05 14:58:27 +0530

    Added bootstrap and jobs are lokking up

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class Blog (models.Model):
+    title = models.CharField(max_length=200)
+    pub_dt = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+
